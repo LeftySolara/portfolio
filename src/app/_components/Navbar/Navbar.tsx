f310a78997ee33abb5bc47dee664f22a2b336fdd
Julianne Adams <julianne@julianneadams.info>
2023-08-01 17:24:53 -0400

    Add: navbar component

---
+++ src/app/_components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <Link href="/">Julianne's Portfolio</Link>
+      </div>
+      <a href="">Resume</a>
+    </div>
+  );
+};
+
+export default Navbar;
