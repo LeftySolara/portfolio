1d1505f047e322eb65d9153a9553e64d31231008
Julianne Adams <julianne@julianneadams.info>
2023-08-03 13:02:40 -0400

    Add: space above footer

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import SocialLinks from "@/app/_components/SocialLinks/SocialLinks";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center w-full">
+    <div className="flex justify-center w-full mt-8">
       <div className="flex flex-col w-10/12">
         <hr className="m-auto w-full" />
         <div className="flex flex-row justify-between p-4">
