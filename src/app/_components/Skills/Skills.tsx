575bdedd1017664a911f1f25213c39c3191fed53
Julianne Adams <julianne@julianneadams.info>
2023-08-06 15:04:51 -0400

    Refactor: make skills section responsive

--- src/app/_components/Skills/Skills.tsx
+++ src/app/_components/Skills/Skills.tsx
@@ -11,7 +11,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SkillCard = ({ name, icon }: { name: string; icon: IconDefinition }) => {
   return (
-    <div className="flex flex-row justify-center bg-secondary text-orange-50 font-semibold border-none rounded-xl w-3/4 px-10 py-3">
+    <div className="flex flex-row justify-center bg-secondary text-orange-50 font-semibold border-none rounded-xl w-3/4 lg:w-5/6 px-10 py-3">
       <FontAwesomeIcon icon={icon} size="xl" />
       <p className="ml-3">{name}</p>
     </div>
@@ -24,7 +24,7 @@ const Skills = () => {
       <h2 className="font-bold font-heading self-center text-green-600 text-3xl m-4">
         Skills
       </h2>
-      <div className="grid grid-rows-3 grid-cols-2 gap-y-5 justify-items-center w-full">
+      <div className="grid grid-rows-3 grid-cols-2 gap-y-5 justify-items-center self-center w-full lg:w-1/2">
         <SkillCard name="HTML" icon={faHtml5} />
         <SkillCard name="CSS" icon={faCss3} />
         <SkillCard name="JavaScript" icon={faJsSquare} />
